bde464fc1dc1acadbe7708e8855629dbbfe170f5
Jens Kristian Espevik <jenskristian@jenske.net>
2024-03-10 12:31:02 +0100

    innitial commit

---
+++ src/main/java/net/jenske/hyttebooking/HyttebookingApplication.java
@@ -0,0 +1,13 @@
+package net.jenske.hyttebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyttebookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyttebookingApplication.class, args);
+	}
+
+}
